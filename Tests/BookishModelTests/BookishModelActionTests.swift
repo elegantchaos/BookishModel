d94141fd86fa735a9e43ef75da66fb0fe19735aa
Sam Deane <sam@elegantchaos.com>
2018-11-22 13:38:56 +0000

    Added NewPerson action test

--- Tests/BookishModelTests/BookishModelActionTests.swift
+++ Tests/BookishModelTests/BookishModelActionTests.swift
@@ -3,11 +3,38 @@
 //  All code (c) 2018 - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-import XCTest
 @testable import BookishModel
+import XCTest
+import CoreData
+import Actions
 
-class BookishModelActionTests: XCTestCase {
-    func testTest() {
+class BookishModelActionTests: ModelTestCase {
+    var context: NSManagedObjectContext!
+    var container: NSPersistentContainer!
+    var actionManager: ActionManager!
+    var info: ActionContext.Info = [:]
+    
+    override func setUp() {
+        container = makeTestContainer()
+        context = container.viewContext
+        actionManager = ActionManager()
+        info[ActionContext.modelKey] = context
+    }
+    
+    func count(of type: String, in context: NSManagedObjectContext? = nil) -> Int {
+        let requestContext = context != nil ? context! : (self.context as NSManagedObjectContext)
+        let request: NSFetchRequest<NSManagedObject> = NSFetchRequest(entityName: type)
+        if let result = try? requestContext.fetch(request) {
+            return result.count
+        }
+        return NSNotFound
+    }
+    
+    func testNewPerson() {
+        actionManager.register(PersonAction.standardActions())
         
+        actionManager.perform(identifier: "NewPerson", sender: self, info: info)
+        
+        XCTAssertEqual(count(of: "Person"), 1)
     }
 }
