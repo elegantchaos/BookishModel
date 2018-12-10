c668ca50ee5761b1f39c137544223b0b36fba460
Sam Deane <sam@elegantchaos.com>
2018-12-10 15:57:25 +0000

    Slightly better mechanism for generating platform-specific versions of the model project dependencies. Fixed tests for iOS

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.0.11"),
         .package(url: "https://github.com/elegantchaos/Actions", from: "1.1.0"),
-        .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.2"),
+        .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.3"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.1"),
     ],
     targets: [
