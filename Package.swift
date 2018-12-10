9da67aeb2938e3c3de918a7dd8db2208254be875
Sam Deane <sam@elegantchaos.com>
2018-12-10 18:11:47 +0000

    Updated to Actions 1.2.0.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "BookishModelDependencies",
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.0.11"),
-        .package(url: "https://github.com/elegantchaos/Actions", from: "1.1.0"),
+        .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.0"),
         .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.3"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.1"),
     ],
