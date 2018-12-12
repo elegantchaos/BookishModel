6b30623ccbc5d97dd40bee39d9bb19f00a06226d
Sam Deane <sam@elegantchaos.com>
2018-12-12 10:57:12 +0000

    Updated coverage version, scripts, etc.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.1.1"),
         .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.0"),
         .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.3"),
-        .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.1"),
+        .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.2"),
     ],
     targets: [
         .target(
