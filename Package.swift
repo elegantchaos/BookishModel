521de6139c0e3adca0e579fb22452d44299279d3
Sam Deane <sam@elegantchaos.com>
2018-12-14 18:11:48 +0000

    Updated the actions branch we're using.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "BookishModelDependencies",
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.2.0"),
-        .package(url: "https://github.com/elegantchaos/Actions", .branch("improved-logging")),
+        .package(url: "https://github.com/elegantchaos/Actions", .branch("fix-multiple-observers")),
         .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.3"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.2"),
     ],
