f7a6f4b9c581aeb9bb1972df531c2504a9473753
Sam Deane <sam@elegantchaos.com>
2018-12-19 18:26:10 +0000

    Made image caching generic.

--- Sources/BookishCore/ImageCache.swift
+++ Sources/BookishCore/ImageCache.swift
@@ -3,59 +3,59 @@
 //  All code (c) 2018 - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-//class ImageFactory<ImageClass> {
-//    class func image(from data: Data) -> ImageClass? {
-//        return nil
-//    }
-//}
-
 #if os(macOS)
-
 import AppKit
-
 #endif
 
 #if os(iOS)
-
 import UIKit
-
-//extension ImageFactory where ImageClass == UIImage {
-//    class func image(from data: Data) -> ImageClass? {
-//         return UIImage(data: data)
-//    }
-//}
-
 #endif
 
 public class ImageCache<ImageClass> {
     let queue = DispatchQueue(label: "image-cache")
     public typealias ImageCallback = (ImageClass) -> Void
     
-    public init() {
-    }
-    
     public func image(for url: URL, callback: @escaping ImageCallback) {
         queue.async {
-            if let data = try? Data(contentsOf: url) {
-                if let image: ImageClass = ImageCache.image(from: data) {
+            do {
+                let data = try Data(contentsOf: url)
+                if let image: ImageClass = self.image(from: data) {
                     DispatchQueue.main.async {
                         callback(image)
                     }
                 }
+            } catch {
+                print("failed to load image \(url)")
             }
         }
     }
     
-    class func image(from data: Data) -> ImageClass? {
+    func image(from data: Data) -> ImageClass? {
         return nil
     }
 }
 
 #if os(iOS)
 
-extension ImageCache where ImageClass == UIImage {
-    class func image(from data: Data) -> ImageClass? {
+public class UIImageCache: ImageCache<UIImage> {
+    override public init() {
+    }
+    
+    override func image(from data: Data) -> UIImage? {
         return UIImage(data: data)
+    }
+}
+
+#endif
+
+#if os(macOS)
+
+public class NSImageCache: ImageCache<NSImage> {
+    override public init() {
+    }
+    
+    override func image(from data: Data) -> NSImage? {
+        return NSImage(data: data)
     }
 }
 
