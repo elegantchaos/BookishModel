ef8ead17e09994bdef11ae46cff61f88a967a9f6
Sam Deane <sam@elegantchaos.com>
2019-01-10 16:06:51 +0000

    Added some less ambiguous convenience methods for creating new instances, and making fetch requests.

--- Sources/BookishModel/ModelObject.swift
+++ Sources/BookishModel/ModelObject.swift
@@ -8,6 +8,19 @@ import CoreData
 public class ModelObject: NSManagedObject {
     static let missingUUID = UUID() as NSUUID
     
+    convenience init(in context: NSManagedObjectContext) {
+//        let entityName = "\(type(of: self))" // TODO: there must be a cleaner way...
+//        let description = context.persistentStoreCoordinator?.managedObjectModel.entitiesByName[entityName]!
+        let description = ModelObject.desc(for: type(of: self), context: context)
+        self.init(entity: description, insertInto: context)
+    }
+    
+    public class func desc<T>(for type: T, context: NSManagedObjectContext) -> NSEntityDescription {
+        let name = "\(type)"
+        let description = context.persistentStoreCoordinator?.managedObjectModel.entitiesByName[name]!
+        return description!
+    }
+    
     public var uniqueIdentifier: NSObject {
         get {
             if let uuid = self.value(forKey: "uuid") as? NSUUID {
@@ -16,5 +29,14 @@ public class ModelObject: NSManagedObject {
                 return ModelObject.missingUUID
             }
         }
+    }
+    
+    public class func fetcher<T>(in context: NSManagedObjectContext) -> NSFetchRequest<T> where T: ModelObject {
+        let description = T.desc(for: T.self, context: context)
+//        let entityName = "\(T.self)" // TODO: there must be a cleaner way...
+//        let description = context.persistentStoreCoordinator?.managedObjectModel.entitiesByName[entityName]!
+        let request = NSFetchRequest<T>()
+        request.entity = description
+        return request
     }
 }
