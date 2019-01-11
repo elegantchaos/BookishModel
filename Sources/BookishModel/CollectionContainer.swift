60f57a785495111e3239218ac03a7bfb7a4a448f
Sam Deane <sam@elegantchaos.com>
2019-01-11 14:37:01 +0000

    Fixed sample data, renamed back to CollectionContainer.

--- Sources/BookishModel/CollectionContainer.swift
+++ Sources/BookishModel/CollectionContainer.swift
@@ -5,9 +5,11 @@
 
 import CoreData
 
-@objc open class BookishCollection: NSPersistentContainer {
-    public typealias LoadedCallback = (BookishCollection, Error?) -> Void
-    public init(url: URL? = nil, usingSample: Bool = false, callback: LoadedCallback? = nil) {
+@objc open class CollectionContainer: NSPersistentContainer {
+    public typealias LoadedCallback = (CollectionContainer, Error?) -> Void
+    
+    public init(url: URL? = nil, usingSample: Bool = false, addTestData: Bool = false, callback: LoadedCallback? = nil) {
+        let fm = FileManager.default
         let model = BookishModel.model()
         super.init(name: "Default", managedObjectModel: model)
         let description = persistentStoreDescriptions[0]
@@ -18,16 +20,28 @@ import CoreData
             description.url = explicitURL
         }
 
+        if let url = description.url {
+            if !fm.fileExists(atPath: url.path) && usingSample {
+                if let sample = Bundle.main.url(forResource: "Sample", withExtension: "sqlite") {
+                    do {
+                        try fm.copyItem(at: sample, to: url)
+                    } catch {
+                        print("failed to copy sample database")
+                    }
+                }
+            }
+        }
+        
         loadPersistentStores { (description, error) in
             if error == nil {
                 let context = self.viewContext
                 context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
                 
-                if usingSample {
+                if addTestData {
                     let request: NSFetchRequest<Book> = Book.fetcher(in: context)
                     if let results = try? context.fetch(request) {
                         if results.count == 0 {
-                            BookishCollection.setupTestDocument(context: context)
+                            self.setupTestData()
                         }
                     }
                 }
@@ -35,36 +49,20 @@ import CoreData
             callback?(self, error)
             }
         }
-//        let url = persistentStoreDescriptions[0].url
-//        let description = NSPersistentStoreDescription(url: url!)
-//        description.options = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
-//        .options[NSMigratePersistentStoresAutomaticallyOption] = true
     }
     
     public var managedObjectContext: NSManagedObjectContext { return viewContext }
     
-//    public func configure(for url: URL) {
-//        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: BookishModel.model())
-//        let options =
-//        try! coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: options)
-//        managedObjectContext.persistentStoreCoordinator = coordinator
-//        managedObjectContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-//    }
-    
-    func setupTestDocument() {
-        BookishCollection.setupTestDocument(context: managedObjectContext)
-    }
-    
     /**
      Populate the document with some test data.
      */
-    
-    public class func setupTestDocument(context: NSManagedObjectContext) {
+
+    func setupTestData() {
+        let context = viewContext
+        
         let formatter = DateFormatter()
         formatter.setLocalizedDateFormatFromTemplate("dd/MM/yy")
         
-//        let desc = context.persistentStoreCoordinator?.managedObjectModel.entitiesByName["Person"]!
-//        let sharedEditor = NSManagedObject(entity: desc!, insertInto: context) as! Person
         let sharedEditor = Person(context: context)
         sharedEditor.name = "Ms Editor"
         sharedEditor.notes = "This person is the editor of a number of books."
@@ -74,22 +72,22 @@ import CoreData
         book.name = "A Book"
         book.notes = "Some\nmulti\nline\nnotes."
         entry.addToBooks(book)
-
+        
         let publisher = Publisher(context: context)
         publisher.notes = "Some notes about the publisher"
         publisher.addToBooks(book)
-
+        
         sharedEditor.relationship(as: Role.Default.authorName).addToBooks(book)
         sharedEditor.relationship(as: Role.Default.illustratorName).addToBooks(book)
-
+        
         let sharedPublisher = Publisher(context: context)
         sharedPublisher.name = "Publisher 2"
         sharedPublisher.notes = "Some notes about the publisher"
-
+        
         let series = Series(context: context)
         series.name = "Example Series"
         series.notes = "Some notes about the series"
-
+        
         
         for n in 1...3 {
             let book = Book(context: context)
@@ -112,5 +110,6 @@ import CoreData
             series.addToEntries(entry)
         }
     }
+    
     
 }
