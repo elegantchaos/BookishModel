1f27ca26a8a5036cdbcb7772f5dc3e167b9470fb
Sam Deane <sam@elegantchaos.com>
2019-02-01 15:15:06 +0000

    Switched to JSONDump 1.0.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.1"),
         .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.2"),
-        .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.3"),
+        .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.2"),
     ],
     targets: [
