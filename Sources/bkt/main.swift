054562d5cce812653f01bf1c458f66d469848293
Sam Deane <sam@elegantchaos.com>
2019-02-18 15:56:36 +0000

    Created a stub command line tool.

---
+++ Sources/bkt/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  bkt
+//
+//  Created by Sam Deane on 18/02/2019.
+//  Copyright © 2019 Elegant Chaos Limited. All rights reserved.
+//
+
+import BookishModel
+
+BookishModel.registerLocalizations()
+print("Hello, World!")
+
