a1b59fa61ead7ff0aa2f2a5c3b4a2e3b2b601139
Sam Deane <sam@elegantchaos.com>
2019-02-18 17:13:37 +0000

    Added very basic model manipulation to bkt

--- Sources/bkt/main.swift
+++ Sources/bkt/main.swift
@@ -7,7 +7,37 @@
 //
 
 import BookishModel
+import Foundation
+import Actions
+import CoreData
 
 BookishModel.registerLocalizations()
-print("Hello, World!")
+
+let url = URL(fileURLWithPath: "test.sql")
+let model = BookishModel.model()
+let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+try! coordinator.destroyPersistentStore(at: url, ofType: "binary")
+
+let container = CollectionContainer(name: "test", url: url) { (container, error) in
+    let context = container.managedObjectContext
+    container.save()
+    
+    let manager = ActionManager()
+    manager.register(ModelAction.standardActions())
+    
+    let info = ActionInfo()
+    info.registerNotification(notification: { (stage, actionContext) in
+        if stage == .didPerform {
+            print(context.countEntities(type: Book.self))
+            print("done")
+            exit(0)
+        }
+    })
+
+    info[ActionContext.modelKey] = context
+    manager.perform(identifier: "NewBook", info: info)
+}
+
+
+dispatchMain()
 
