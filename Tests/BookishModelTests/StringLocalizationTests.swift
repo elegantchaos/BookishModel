c016868373eecb575cec6dd276a6ec01458944ba
Sam Deane <sam@elegantchaos.com>
2019-02-28 16:42:39 +0000

    More string localization tests.

--- Tests/BookishModelTests/StringLocalizationTests.swift
+++ Tests/BookishModelTests/StringLocalizationTests.swift
@@ -21,4 +21,19 @@ class StringLocalizationTests: ModelTestCase {
         XCTAssertEqual("parameters".localized(with: ["name": "Fred"]), "Name is Fred.")
 
     }
+    
+    func testCount() {
+        StringLocalization.registerLocalizationBundle(Bundle(for: type(of: self)))
+        XCTAssertEqual("count".localized(count: 0), "none")
+        XCTAssertEqual("count".localized(count: 1), "one")
+        XCTAssertEqual("count".localized(count: 2), "2")
+    }
+
+    func testCountSelection() {
+        StringLocalization.registerLocalizationBundle(Bundle(for: type(of: self)))
+        XCTAssertEqual("selection".localized(count: 2, selected: 0), "0 of 2 selected")
+        XCTAssertEqual("selection".localized(count: 2, selected: 1), "1 of 2 selected")
+        XCTAssertEqual("selection".localized(count: 2, selected: 2), "All 2 selected")
+    }
+
 }
