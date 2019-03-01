4fe254b085b7ef853d616ea082ff1877541688ec
Sam Deane <sam@elegantchaos.com>
2019-03-01 14:56:29 +0000

    Added test with valid isbn10 containing x

--- Sources/BookishModel/Utilities/String+ISBN.swift
+++ Sources/BookishModel/Utilities/String+ISBN.swift
@@ -6,42 +6,38 @@
 import Foundation
 
 extension String {
-    static let checkCharacters = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 88]
-    
-    var isISBN10: Bool {
-        guard self.count == 10 else {
-            return false
-        }
+    static let checkDigits: [Int8] = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 88]
 
-        guard let chars = self.cString(using: .ascii) else {
-            return false
-        }
+    static func checkDigitISBN10(for chars: ArraySlice<CChar>) -> Int8 {
+        assert(chars.count == 9)
 
         let zeroChar = Int8(48)
         var sum = 0
         var n = 10
-        for c in chars[..<9] {
+        for c in chars {
             let digit = c - zeroChar
             sum += n * Int(digit)
             n -= 1
         }
         
-        let calculatedChecksum = String.checkCharacters[11 - (sum % 11)]
-        let existingChecksum = chars[9]
+        let calculatedChecksum = String.checkDigits[11 - (sum % 11)]
+        return Int8(calculatedChecksum)
+    }
+    
+    var isISBN10: Bool {
+        guard self.count == 10, let chars = self.cString(using: .ascii) else {
+            return false
+        }
+
+        let range = chars[..<9]
+        let calculatedDigit = String.checkDigitISBN10(for: range)
+        let existingDigit = chars[9]
         
-        return calculatedChecksum == existingChecksum
+        return calculatedDigit == existingDigit
     }
 
     var isISBN13: Bool {
         return self.count == 10
-    }
-
-    var checksumISBN10: Character {
-        return "0"
-    }
-
-    var checksumISBN13: Character {
-        return "0"
     }
 
 }
--- Tests/BookishModelTests/ISBNTests.swift
+++ Tests/BookishModelTests/ISBNTests.swift
@@ -7,18 +7,28 @@ import XCTest
 @testable import BookishModel
 
 class ISBNTests: ModelTestCase {
+    let valid10 = "3200328584"
+    let valid10withX = "013603599X"
+    let valid13 = "9783200328587"
+    let invalid13 = "1233200328587"
 
     func testIsISBN10() {
-        XCTAssertTrue("3200328584".isISBN10)
-        XCTAssertFalse("9783200328587".isISBN10)
-        XCTAssertFalse("1233200328587".isISBN10)
+        XCTAssertTrue(valid10.isISBN10)
+        XCTAssertTrue(valid10withX.isISBN10)
+        
+        XCTAssertFalse(valid13.isISBN10)
+        XCTAssertFalse(invalid13.isISBN10)
+        
         XCTAssertFalse("blah".isISBN10)
     }
     
     func testIsISBN13() {
-        XCTAssertFalse("3200328584".isISBN13)
-        XCTAssertTrue("9783200328587".isISBN13)
-        XCTAssertFalse("1233200328587".isISBN13)
+        XCTAssertFalse(valid10.isISBN13)
+        XCTAssertFalse(valid10withX.isISBN13)
+        
+        XCTAssertTrue(valid13.isISBN13)
+        XCTAssertFalse(invalid13.isISBN13)
+        
         XCTAssertFalse("blah".isISBN13)
     }
 
