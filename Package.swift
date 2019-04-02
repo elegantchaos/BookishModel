a3f630c97da31c4eb7d724ccb2cf2af799cf28c1
Sam Deane <sam@elegantchaos.com>
2019-04-02 17:37:04 +0100

    Upgraded to Actions 1.2.4.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.2"),
-        .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.3"),
+        .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.4"),
         .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.2"),
     ],
