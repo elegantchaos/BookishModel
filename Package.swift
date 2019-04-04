bf29c803b2327fe8bec3885085b1db56255f45d2
Sam Deane <sam@elegantchaos.com>
2019-04-04 11:13:15 +0100

    updated to Logger 1.3.3. Bumped up the timeout for the fatal error tests.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["BookishCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.2"),
+        .package(url: "https://github.com/elegantchaos/Logger", from: "1.3.3"),
         .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.4"),
         .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.3"),
