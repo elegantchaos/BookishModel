60744f168c87419d91b1d68b87a94ea03c884856
Sam Deane <sam@elegantchaos.com>
2019-05-09 16:24:51 +0100

    Improved logging.

--- Sources/BookishModel/NavigationStack.swift
+++ Sources/BookishModel/NavigationStack.swift
@@ -7,7 +7,7 @@ import Logger
 
 let navigationStackChannel = Logger("com.elegantchaos.bookish.model.NavigationStack")
 
-public class NavigationStack<ItemType> {
+public class NavigationStack<ItemType>: CustomStringConvertible {
     internal var stack: [ItemType]
     internal var position: Int
     
@@ -33,37 +33,55 @@ public class NavigationStack<ItemType> {
     }
 
     public func reset(to: ItemType? = nil) {
+        navigationStackChannel.log("Resetting.")
         stack = []
         position = 0
         if let item = to {
             push(item)
         }
+        navigationStackChannel.log("Stack is now:\n\(self)")
     }
     
     public func push(_ item: ItemType) {
-        navigationStackChannel.log("Adding item \(item). Current position \(position).")
+        navigationStackChannel.log("Adding item \(item) at \(position).")
         let count = stack.count - position
         stack.removeLast(count)
         stack.append(item)
         position = stack.count
-        navigationStackChannel.log("New position \(position)")
+        navigationStackChannel.log("Stack is now:\n\(self)")
     }
     
     @discardableResult public func goBack() -> ItemType? {
         guard position > 1 else {
+            navigationStackChannel.log("Can't go back.")
             return nil
         }
 
+        navigationStackChannel.log("Going back.")
         position -= 1
+        navigationStackChannel.log("Stack is now:\n\(self)")
         return stack[position - 1]
     }
 
     @discardableResult public func goForward() -> ItemType? {
         guard position < stack.count else {
+            navigationStackChannel.log("Can't go forward.")
             return nil
         }
         
+        navigationStackChannel.log("Going forward.")
         position += 1
+        navigationStackChannel.log("Stack is now:\n\(self)")
         return stack[position - 1]
+    }
+    
+    public var description: String {
+        var string = ""
+        let selected = position - 1
+        for n in 0 ..< stack.count {
+            string += n == selected ? "--> " : "    "
+            string += "\(stack[n])\n"
+        }
+        return string
     }
 }
