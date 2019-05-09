14f8b011f44f152a0f807c506bd5559a9f2b843b
Sam Deane <sam@elegantchaos.com>
2019-05-09 14:23:04 +0100

    Added navigation stack and tests

---
+++ Tests/BookishModelTests/NavigationStackTests.swift
@@ -0,0 +1,10 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 09/05/2019.
+//  All code (c) 2019 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+class NavigationStackTests {
+    
+}
