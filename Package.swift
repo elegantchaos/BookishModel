91c7c182bad6b76dcfa8a3a57d8772828aa08d17
Sam Deane <sam@elegantchaos.com>
2019-05-17 15:19:20 +0100

    Use bookish branch of actions

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger", .branch("bookish")),
-        .package(url: "https://github.com/elegantchaos/Actions", from: "1.2.4"),
+        .package(url: "https://github.com/elegantchaos/Actions", .branch("bookish")),
         .package(url: "https://github.com/elegantchaos/JSONDump", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/Coverage", from: "1.0.4"),
         .package(url: "https://github.com/elegantchaos/CommandShell", from: "1.0.3"),
