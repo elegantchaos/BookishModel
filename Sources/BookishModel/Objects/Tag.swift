452a78515b1343ee3c61cbb2bfe77651893ac37b
Sam Deane <sam@elegantchaos.com>
2019-05-17 16:10:10 +0100

    Fixed tags in model. Fixed sample data.

---
+++ Sources/BookishModel/Objects/Tag.swift
@@ -0,0 +1,10 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 17/05/2019.
+//  All code (c) 2019 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+public class Tag: ChangeTrackingModelObject {
+}
+
