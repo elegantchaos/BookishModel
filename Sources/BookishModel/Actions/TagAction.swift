41962055af0bede5a85562c913acba4a17968f83
Sam Deane <sam@elegantchaos.com>
2019-05-23 11:40:48 +0100

    Added TagObserver protocol.

--- Sources/BookishModel/Actions/TagAction.swift
+++ Sources/BookishModel/Actions/TagAction.swift
@@ -6,6 +6,18 @@
 import CoreData
 import Actions
 
+public protocol TagObserver: ActionObserver {
+    func deleted(tags: Set<Tag>)
+    func renamed(tags: Set<Tag>)
+    func changed(adding addedTags: Set<Tag>, removing removedTags: Set<Tag>)
+}
+
+public extension TagObserver {
+    func deleted(tags: Set<Tag>) { }
+    func renamed(tags: Set<Tag>) { }
+    func changed(adding addedTags: Set<Tag>, removing removedTags: Set<Tag>) { }
+}
+
 public class TagAction: SyncModelAction {
     public static let addedTagsKey = "added"
     public static let removedTagsKey = "removed"
@@ -31,10 +43,14 @@ public class ChangeTagsAction: TagAction {
                 for tag in removedTags {
                     tag.remove(from: selection)
                 }
+                
                 for tag in addedTags {
                     tag.add(to: selection)
                 }
-
+                
+                context.info.forObservers { (observer: TagObserver) in
+                    observer.changed(adding: addedTags, removing: removedTags)
+                }
         }
     }
 }
@@ -43,6 +59,10 @@ public class DeleteTagAction: TagAction {
     public override func perform(context: ActionContext, model: NSManagedObjectContext) {
         if let tag = context[TagAction.tagKey] as? Tag {
             model.delete(tag)
+            
+            context.info.forObservers { (observer: TagObserver) in
+                observer.deleted(tags: [tag])
+            }
         }
     }
     
@@ -52,6 +72,10 @@ public class RenameTagAction: TagAction {
     public override func perform(context: ActionContext, model: NSManagedObjectContext) {
         if let tag = context[TagAction.tagKey] as? Tag, let name = context[TagAction.tagNameKey] as? String {
             tag.name = name
+            
+            context.info.forObservers { (observer: TagObserver) in
+                observer.renamed(tags: [tag])
+            }
         }
     }
 }
