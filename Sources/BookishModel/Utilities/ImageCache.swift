73a4b1a15f0c65b5d12ecad701d6854097cd65cb
Sam Deane <sam@elegantchaos.com>
2019-08-14 15:58:43 +0100

    Cache images to local storage.

--- Sources/BookishModel/Utilities/ImageCache.swift
+++ Sources/BookishModel/Utilities/ImageCache.swift
@@ -3,6 +3,13 @@
 //  All code (c) 2018 - present day, Elegant Chaos Limited.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+import Foundation
+import Logger
+import CryptoKit
+import CommonCrypto
+
+let imageCacheChannel = Channel("com.elegantchaos.imageCache")
+
 public protocol ImageFactory {
     associatedtype ImageClass
     static func image(from data: Data) -> ImageClass?
@@ -11,6 +18,8 @@ public protocol ImageFactory {
 
 public class ImageCache<Factory: ImageFactory> {
     let queue = DispatchQueue(label: "image-cache")
+    let cacheURL = try! FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
     public typealias ImageCallback = (Factory.ImageClass) -> Void
     
     public init() {
@@ -19,16 +28,54 @@ public class ImageCache<Factory: ImageFactory> {
     public func image(for url: URL, callback: @escaping ImageCallback) {
         queue.async {
             do {
-                let data = try Data(contentsOf: url)
-                if let image = Factory.image(from: data) {
+                let localURL = self.localURL(for: url)
+                if let image = self.load(cachedURL: localURL) ?? self.load(remoteURL: url, toLocalURL: localURL) {
                     DispatchQueue.main.async {
                         callback(image)
                     }
+                } else {
+                    print("failed to load image \(url)")
                 }
-            } catch {
-                print("failed to load image \(url)")
             }
         }
+    }
+    
+    private func load(cachedURL url: URL) -> Factory.ImageClass? {
+        guard let data = try? Data(contentsOf: url) else {
+            return nil
+        }
+        
+        guard let image = Factory.image(from: data) else {
+            try? FileManager.default.removeItem(at: url)
+            return nil
+        }
+        
+        imageCacheChannel.log("loaded \(url.lastPathComponent) from cache")
+        return image
+    }
+    
+    private func load(remoteURL url: URL, toLocalURL: URL) -> Factory.ImageClass? {
+        guard let data = try? Data(contentsOf: url), let image = Factory.image(from: data) else {
+            return nil
+        }
+
+        do {
+            try data.write(to: toLocalURL)
+            imageCacheChannel.log("cached \(url.lastPathComponent) from cache")
+        } catch {
+            imageCacheChannel.log("couldn't cache \(url.lastPathComponent)\n\(error)")
+        }
+        
+        return image
+    }
+    
+    private func localURL(for imageURL: URL) -> URL {
+        let imageCachedName = imageURL.absoluteString
+        let imageCachedData = imageCachedName.data(using: .utf8)!
+        let hashedName = CryptoKit.SHA256.hash(data: imageCachedData)
+        let digest = hashedName.description
+        let cachedImageURL = cacheURL.appendingPathComponent(digest)
+        return cachedImageURL
     }
 }
 
